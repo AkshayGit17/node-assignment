2572d05850047cd24b6b35c896ccbbbd60680960
Akshay Kini <akshay.kini@niveussolutions.com>
2025-04-17 15:37:25 +0530

    Created file using fs module

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function displayMessage() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Hello from Promise!");
-      }, 1000);
-    });
+const fs = require('fs').promises;
+
+async function createFile() {
+  try {
+    await fs.writeFile('message.txt', 'This file was created using fs module and async/await.');
+    console.log('File created successfully.');
+  } catch (error) {
+    console.error('Error creating file:', error);
   }
-  
-  displayMessage().then((message) => {
-    console.log(message);
+}
+
+function displayMessage() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from Promise!");
+    }, 1000);
   });
+}
+
+(async () => {
+  const message = await displayMessage();
+  console.log(message);
+
+  await createFile();
+})();
